9068c6e3559eceba716bbc9df52d888d900beebd
orion <orionriitters@gmail.com>
2019-12-18 00:33:00 -0600

    init

---
+++ src/components/containers/landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.scss'
+
+export default function Landing() {
+  return (
+    <div className="Landing">
+      <div className="page-header">
+        Hey!
+      </div>
+    </div>
+  );
+}
